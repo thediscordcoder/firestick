753f2981b857cab3572f710c5874b6e6f52046fd
imperfect <imperfect@29b0464b-1456-0410-ae1d-5988085663d1>
2008-09-19 20:55:22 +0000

    Figured out where to get profiling info, put that in main library.

--- application/libraries/Firestick.php
+++ application/libraries/Firestick.php
@@ -38,7 +38,31 @@ class Firestick {
      * If we are logging this run, write profiling results to DB.
      */
     function resolve_profiling() {
-        print '<h3>resolve_profiling()</h3>';
+        // Total elapsed script time
+        $elapsed = $this->CI->benchmark->elapsed_time('total_execution_time_start', 'total_execution_time_end');
+        print '<h3>Elapsed: ' . $elapsed . '</h3>';
+        
+        // Total consumed memory
+        $memory	 = ( ! function_exists('memory_get_usage')) ? 0 : memory_get_usage();
+        print '<h3>Memory: ' . $memory . '</h3>';
+        
+        // DB calls
+        if ( ! class_exists('CI_DB_driver')) {
+            print 'NO DB LOADED';
+        }
+        else {
+			if (count($this->CI->db->queries) == 0) {
+                print 'NO DB CALLS';
+            }
+            else {
+                foreach ($this->CI->db->queries as $key => $val) {
+					$val = htmlspecialchars($val, ENT_QUOTES);
+					$time = number_format($this->CI->db->query_times[$key], 4);
+                    
+                    print 'DB Call: ' . $key . ' => ' . $val . ', Time: ' . $time . '<br />';
+                }
+            }
+        }
     }
 }
 
