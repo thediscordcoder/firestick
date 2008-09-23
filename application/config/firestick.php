5fbc72877ec5bab8d332f91f72af73a624aecba2
imperfect <imperfect@29b0464b-1456-0410-ae1d-5988085663d1>
2008-09-23 14:43:54 +0000

    Properly added the GPL.

--- application/config/firestick.php
+++ application/config/firestick.php
@@ -1,4 +1,28 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+/**
+ * Main library file for Firestick.
+ *
+ * @author Dan Hulton <dan@danhulton.com>
+ * @package firestick_config_package
+ * @copyright Copyright 2008, Dan Hulton
+ *
+ *  This file is part of FireStick.
+ *
+ *  Foobar is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  FireStick is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with FireStick.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 /*
 |--------------------------------------------------------------------------
